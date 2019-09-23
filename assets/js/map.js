ea85e6d67a42a786151c373ed984cfb2eba35b6b
Ubuntu <ubuntu@ip-172-31-85-89.ec2.internal>
2019-09-23 15:46:26 +0000

    Map styled. Modal created.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,26 @@
-$(function() {
-    $('#map').vectorMap({ map: 'world_mill' });
+$(document).ready(function() {
+
+    $(function() {
+        var map = new jvm.Map({
+            map: 'world_mill',
+            container: $('#map'),
+            backgroundColor: 'rgb(52,58,64)',
+
+            regionStyle: {
+                initial: {
+                    fill: 'rgb(197, 179, 88)'
+                }
+            },
+
+            onRegionClick: function(event, code) {
+                var targetCountry = map.getRegionName(code);
+                console.log(targetCountry);
+                console.log(code);
+            }
+
+        });
+        // Store selected regions locally
+        map.setSelectedRegions(JSON.parse(window.localStorage.getItem('jvectormap-selected-regions') || '[]'));
+    });
+
 });
