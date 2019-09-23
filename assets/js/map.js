ee618e1ced892652b97ac475761968d1d7306875
Ubuntu <ubuntu@ip-172-31-85-89.ec2.internal>
2019-09-23 15:48:49 +0000

    Modal function created

--- assets/js/map.js
+++ assets/js/map.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
                 var targetCountry = map.getRegionName(code);
                 console.log(targetCountry);
                 console.log(code);
+                mapModal();
             }
 
         });
@@ -23,4 +24,48 @@ $(document).ready(function() {
         map.setSelectedRegions(JSON.parse(window.localStorage.getItem('jvectormap-selected-regions') || '[]'));
     });
 
+
+    // Modal Function
+    function mapModal() { //Based on: https://www.w3schools.com/howto/howto_css_modals.asp
+        // Display modal
+        $("#myModal").fadeIn(500);
+
+        $('#visited').click(function() {
+            closeModal();
+        });
+
+        $('#not-visited').click(function() {
+            closeModal();
+        });
+
+        $('#plan-to-visit').click(function() {
+            closeModal();
+        });
+
+        $('#will-not-visit').click(function() {
+            closeModal();
+        });
+
+        // When the user clicks on <span> (x), close the modal
+        $(".closeModal").click(function() {
+            closeModal();
+        });
+
+        // When user clicks button, close modal
+        $("#closeBtn").click(function() {
+            closeModal();
+        });
+
+        // When the user clicks anywhere outside of the modal content, close it
+        $(window).click(function(event) {
+            if ($(event.target).is('#myModal')) {
+                closeModal();
+            }
+        });
+    }
+
+    // Close modal function
+    function closeModal() {
+        $("#myModal").fadeOut(500);
+    }
 });
