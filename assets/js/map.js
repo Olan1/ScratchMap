376943eae72a54a8be0808521c09306510eb6c07
Ubuntu <ubuntu@ip-172-31-85-89.ec2.internal>
2019-09-29 22:27:52 +0000

    Added board section. Added board bubble function

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,106 +1,136 @@
 $(document).ready(function() {
+
+    // Load JSON data
     $.getJSON('assets/data/data.json', function(data) { // Source: http://jvectormap.com/examples/france-elections/
-        var colorData = data.data;
+        // Assign color values
+        var colorData = data.data.color;
 
+        // Call draw map function
+        map(colorData);
 
-        $(function() {
-            // Call draw map function
-            map(colorData);
-            // Draw map function
-            function map(data) {
-                $('#map').vectorMap({
-                    map: 'world_mill',
-                    container: $('#map'),
-                    backgroundColor: "#343a40",
-                    series: {
-                        regions: [{
-                            values: colorData,
-                            scale: ['#c5b358', '#3bd80d', '#1cb2ed', '#fe0000'],
-                            normalizeFunction: 'polynomial'
-                        }]
-                    },
-                    onRegionClick: function(event, code) {
-                        mapModal(code);
-                    }
-                });
-            }
+        // Draw map function
+        function map(data) {
+            $('#map').vectorMap({
+                map: 'world_mill',
+                container: $('#map'),
+                backgroundColor: "#343a40",
+                series: {
+                    regions: [{
+                        values: colorData,
+                        scale: ['#c5b358', '#3bd80d', '#1cb2ed', '#fe0000'],
+                        normalizeFunction: 'polynomial'
+                    }]
+                },
+                onRegionClick: function(event, code) {
+                    var map = $('#map').vectorMap('get', 'mapObject'); // Source: https://stackoverflow.com/questions/36035810/want-to-get-region-name-from-drill-down-map-of-jvectormap
+                    var name = map.getRegionName(code);
+                    mapModal(code, name);
+                }
+            });
+        }
 
-            // Modal Function
-            function mapModal(regionCode) { //Based on modal from https://www.w3schools.com/howto/howto_css_modals.asp
-                // Display modal
-                $("#myModal").fadeIn(500);
+        // Modal Function
+        function mapModal(regionCode, regionName) { //Based on modal from https://www.w3schools.com/howto/howto_css_modals.asp
+            // Display modal
+            $("#myModal").fadeIn(500);
 
-                // When the user clicks on <span> (x), close the modal
-                $(".closeModal").click(function() {
-                    closeModal();
-                });
+            $("#modalHeader").text(regionName);
 
-                // When user clicks button, close modal
-                $("#closeBtn").click(function() {
-                    closeModal();
-                });
-
-                // When the user clicks anywhere outside of the modal content, close it
-                $(window).click(function(event) {
-                    if ($(event.target).is('#myModal')) {
-                        closeModal();
-                    }
-                });
-
-                // Call modal button click events function
-                modalBtnClick(regionCode);
-            }
-
-            // Close modal function
-            function closeModal() {
-                $("#myModal").fadeOut(500);
-            }
-
-            // Modal button click events
-            function modalBtnClick(regionCode) {
-                $('#home').off('click').on('click', function() {
-                    Object.keys(colorData).forEach(function(item) { // Source: https://gomakethings.com/the-es6-way-to-loop-through-objects-with-vanilla-javascript/
-                        if (colorData[item] == 2.5) {
-                            colorData[item] = 1; // Gold
-                        }
-                    });
-                    colorData[regionCode] = 2.5; // Green
-                    redrawMap();
-                });
-
-                $('#resided').off('click').on('click', function() {
-                    colorData[regionCode] = 10; // Turquoise
-                    redrawMap();
-                });
-
-                $('#visited').off('click').on('click', function() {
-                    colorData[regionCode] = 18; // Blue
-                    redrawMap();
-                });
-
-                $('#not-visited').off('click').on('click', function() {
-                    colorData[regionCode] = 1; //Gold
-                    redrawMap();
-                });
-
-                $('#plan-to-visit').off('click').on('click', function() {
-                    colorData[regionCode] = 36; // Purple
-                    redrawMap();
-                });
-
-                $('#will-not-visit').off('click').on('click', function() {
-                    colorData[regionCode] = 55; // Red
-                    redrawMap();
-                });
-            }
-
-            // Redraw map function
-            function redrawMap() {
-                $("#map").vectorMap('get', 'mapObject').remove(); // Source: https://stackoverflow.com/questions/31868444/jvectormap-change-refresh-map-with-new-reference-map
-                map(colorData);
+            // When the user clicks on <span> (x), close the modal
+            $(".closeModal").click(function() {
                 closeModal();
-            }
+            });
 
-        });
+            // When user clicks button, close modal
+            $("#closeBtn").click(function() {
+                closeModal();
+            });
+
+            // When the user clicks anywhere outside of the modal content, close it
+            $(window).click(function(event) {
+                if ($(event.target).is('#myModal')) {
+                    closeModal();
+                }
+            });
+
+            // Call modal button click events function
+            modalBtnClick(regionCode, regionName);
+        }
+
+        // Close modal function
+        function closeModal() {
+            $("#myModal").fadeOut(500);
+        }
+
+        // Modal button click events
+        function modalBtnClick(regionCode, regionName) {
+            $('#home').off('click').on('click', function() {
+                Object.keys(colorData).forEach(function(item) { // Source: https://gomakethings.com/the-es6-way-to-loop-through-objects-with-vanilla-javascript/
+                    if (colorData[item] == 2.5) {
+                        colorData[item] = 1; // Gold
+                    }
+                });
+                boardBubble('#home', '#homeBoard', regionName, 'home', regionCode);
+                colorData[regionCode] = 2.5; // Green
+                redrawMap();
+            });
+
+            $('#resided').off('click').on('click', function() {
+                boardBubble('#resided', '#livedInBoard', regionName, 'resided', regionCode);
+                colorData[regionCode] = 10; // Turquoise
+                redrawMap();
+            });
+
+            $('#visited').off('click').on('click', function() {
+                boardBubble('#visited', '#visitedBoard', regionName, 'visited', regionCode);
+                colorData[regionCode] = 18; // Blue
+                redrawMap();
+            });
+
+            $('#not-visited').off('click').on('click', function() {
+                boardBubble('#not-visited', 'targetBoard', regionName, 'colorClass', regionCode);
+                colorData[regionCode] = 1; //Gold
+                redrawMap();
+            });
+
+            $('#plan-to-visit').off('click').on('click', function() {
+                boardBubble('#plan-to-visit', '#wantToVisitBoard', regionName, 'wantToVisit', regionCode);
+                colorData[regionCode] = 36; // Purple
+                redrawMap();
+            });
+
+            $('#will-not-visit').off('click').on('click', function() {
+                boardBubble('#will-not-visit', '#willNotVisitBoard', regionName, 'willNotVisit', regionCode);
+                colorData[regionCode] = 55; // Red
+                redrawMap();
+            });
+        }
+
+        // Redraw map function
+        function redrawMap() {
+            $("#map").vectorMap('get', 'mapObject').remove(); // Source: https://stackoverflow.com/questions/31868444/jvectormap-change-refresh-map-with-new-reference-map
+            map(colorData);
+            closeModal();
+        }
+
+        // Add countries to board function
+        function boardBubble(targetBtn, targetBoard, regionName, colorClass, regionCode) {
+
+            if (targetBtn == '#home') {
+                $(targetBoard).find("li").first().remove();
+            }
+            $("li").each(function() {
+                if ($(this).children("div").attr("id") == regionCode) {
+                    $(this).remove();
+                }
+            });
+
+            var bubble = `<li><div class="board-bubble ${colorClass}" id="${regionCode}"><span class="bubble-text">${regionName}</span></div></li>`;
+
+            if (targetBtn != '#not-visited') {
+                $(targetBoard).children("ul").append(bubble);
+            }
+        }
+
     });
 });
